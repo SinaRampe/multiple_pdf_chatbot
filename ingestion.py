4e207976c567015212628d250ea82e1c4dfe1eb4
Sina <sinarampe@gmail.com>
2023-05-03 22:14:28 +0200

    fix ValueError disallowed special token '<|endoftext|>'

--- ingestion.py
+++ ingestion.py
@@ -16,14 +16,14 @@ def ingest_docs() -> None:
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents) } documents")
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
+        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""],
     )
     documents = text_splitter.split_documents(documents=raw_documents)
 
     print(f"Splitted into {len(documents)} chunks")
 
     print(f"Going to insert {len(documents)} to Pinecone")
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(disallowed_special=())
     Pinecone.from_documents(
         documents=documents, embedding=embeddings, index_name="multiple-pdf-chatbot"
     )
