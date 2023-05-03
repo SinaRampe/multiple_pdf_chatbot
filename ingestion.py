f0f3be0d41e09866f718e208522249801a8b573d
Sina <sinarampe@gmail.com>
2023-05-03 21:43:36 +0200

    initial commit

---
+++ ingestion.py
@@ -0,0 +1,33 @@
+import os
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+
+def ingest_docs() -> None:
+    loader = PyPDFDirectoryLoader("data/")
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents) } documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
+    )
+    documents = text_splitter.split_documents(documents=raw_documents)
+
+    print(f"Splitted into {len(documents)} chunks")
+
+    print(f"Going to insert {len(documents)} to Pinecone")
+    embeddings = OpenAIEmbeddings()
+    Pinecone.from_documents(
+        documents=documents, embedding=embeddings, index_name="multiple-pdf-chatbot"
+    )
+    print("Added to Pinecone vectorstore vectors")
+
+if __name__ == "__main__":
+    ingest_docs()
